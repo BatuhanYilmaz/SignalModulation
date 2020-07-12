166e62998c1e0a4851b81698316be5d4c23c81e0
BatuhanYilmaz <batuhanyilmaz@marun.edu.tr>
2020-07-12 18:06:38 +0300

    Update and rename Image_trans_QPSK_16QAM_64QAM.m to Image_trans_QPSK_16PSK_16QAM_64QAM.m

--- MPSK & MQAM/Image_trans_QPSK_16PSK_16QAM_64QAM.m
+++ MPSK & MQAM/Image_trans_QPSK_16PSK_16QAM_64QAM.m
@@ -1,65 +1,50 @@
-
 clc;
 clear all;
 close all;
-%% Loading transmitted signals
-load tx_qpsk.mat
-load tx_16psk.mat
-load tx_16qam.mat
-load tx_64qam.mat
-load input.mat
-
-% Specifying the dimensions
-dim_hor = 729
-dim_vert = 730
-num_of_ch = 3
-N = dim_hor * dim_vert * num_of_ch
-len = 8*N % each channel is represented by 8-bit
 
 %% Modulator and Demodulator Objects %%%
-%h_qpsk = modem.pskmod('M',4,'phaseoffset',pi/4,'inputtype','bit');
+h_qpsk = modem.pskmod('M',4,'phaseoffset',pi/4,'inputtype','bit');
 g_qpsk = modem.pskdemod('M',4,'phaseoffset',pi/4,'outputtype','bit');
 
-%h_16psk = modem.pskmod('M',16,'phaseoffset',pi/4,'inputtype','bit');
+h_16psk = modem.pskmod('M',16,'phaseoffset',pi/4,'inputtype','bit');
 g_16psk = modem.pskdemod('M',16,'phaseoffset',pi/4,'outputtype','bit');
 
-% h_16qam = modem.qammod('M',16,'inputtype','bit');
+ h_16qam = modem.qammod('M',16,'inputtype','bit');
 g_16qam = modem.qamdemod('M',16,'outputtype','bit');
-% h_64qam = modem.qammod('M',64,'inputtype','bit');
+ h_64qam = modem.qammod('M',64,'inputtype','bit');
 g_64qam = modem.qamdemod('M',64,'outputtype','bit');
 %%%% TRANSMITTER   %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%  in=imread('lena.jpg');    % image to be transmitted and matlab code should be in same directory
-%  N=numel(in);
-%  in2=reshape(in,N,1);
-%  bin=de2bi(in2,'left-msb');
-%  input=reshape(bin',numel(bin),1);
-%  len=length(input);
+  in=imread('lena.jpg');    % image to be transmitted and matlab code should be in same directory
+  N=numel(in);
+  in2=reshape(in,N,1);
+  bin=de2bi(in2,'left-msb');
+  input=reshape(bin',numel(bin),1);
+  len=length(input);
 % %%%%% padding zeroes to input %%%
-% z=len;
-% while(rem(z,2) || rem(z,4)|| rem(z,6))
-%     z=z+1;
-%     input(z,1)=0;
-% end
-% input = double(input);
-%  y_qpsk=modulate(h_qpsk,input);
-%  y_16psk = modulate(h_16psk,input);
-% y_16qam=modulate(h_16qam,input);
-% y_64qam=modulate(h_64qam,input);
+ z=len;
+ while(rem(z,2) || rem(z,4)|| rem(z,6))
+     z=z+1;
+     input(z,1)=0;
+ end
+ input = double(input);
+  y_qpsk=modulate(h_qpsk,input);
+  y_16psk = modulate(h_16psk,input);
+ y_16qam=modulate(h_16qam,input);
+ y_64qam=modulate(h_64qam,input);
 
-% ifft_out_qpsk=ifft(y_qpsk);
-% ifft_out_16psk = ifft(y_16psk);
-% ifft_out_16qam=ifft(y_16qam);
-% ifft_out_64qam=ifft(y_64qam);
+ ifft_out_qpsk=ifft(y_qpsk);
+ ifft_out_16psk = ifft(y_16psk);
+ ifft_out_16qam=ifft(y_16qam);
+ ifft_out_64qam=ifft(y_64qam);
 
 
 % %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% SNR = 10;          % SNR in dB
-% tx_qpsk = awgn(ifft_out_qpsk,SNR,'measured');
-% tx_16psk = awgn(ifft_out_16psk,SNR,'measured');
-% tx_16qam=awgn(ifft_out_16qam,SNR,'measured');
-% tx_64qam=awgn(ifft_out_64qam,SNR,'measured');
+ SNR = 10;          % SNR in dB
+ tx_qpsk = awgn(ifft_out_qpsk,SNR,'measured');
+ tx_16psk = awgn(ifft_out_16psk,SNR,'measured');
+ tx_16qam=awgn(ifft_out_16qam,SNR,'measured');
+ tx_64qam=awgn(ifft_out_64qam,SNR,'measured');
 % %%%    RECEIVER  %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
 
 %% QPSK RECEIVER STRUCTURE
 % Discrete Fourier Transform
@@ -74,8 +59,8 @@ dec_qpsk = bi2de(b1,'left-msb');
 % Bit error rate calculation for QPSK
 BER_qpsk = biterr(input,l_qpsk)/len
 % Reconstructed image for QPSK, 729x730x3
-%im_qpsk = reshape(dec_qpsk(1:N),size(in,1),size(in,2),size(in,3));
-im_qpsk=reshape(dec_qpsk(1:N),730,729,3);
+im_qpsk = reshape(dec_qpsk(1:N),size(in,1),size(in,2),size(in,3));
+%im_qpsk=reshape(dec_qpsk(1:N),730,729,3);
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %% 16-PSK RECEIVER STRUCTURE
@@ -131,11 +116,9 @@ subplot(141);
 imshow(im_qpsk);
 title('QPSK');
 
-
 subplot(142);
 imshow(im_qpsk);
 title('16-PSK');
-
 
 subplot(143);
 imshow(im_16qam);
@@ -144,4 +127,3 @@ title('16-QAM');
 subplot(144);
 imshow(im_64qam);
 title('64-QAM');
-
